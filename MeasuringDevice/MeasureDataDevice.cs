752450bca1fbab24ca2636d21130da901958216a
Илья Абрамёнок <i.abramenok@slata.com>
2022-06-09 18:29:20 +0800

    Добавьте файлы проекта.

---
+++ MeasuringDevice/MeasureDataDevice.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DeviceControl;
+
+namespace MeasuringDevice
+{
+    public abstract class MeasureDataDevice : IMeasuringDevice
+    {
+        public abstract decimal MetricValue();
+
+        public abstract decimal ImperialValue();
+
+        public void StartCollecting()
+        {
+            controller = DeviceController.StartDevice(measurementType);
+            GetMeasurements();
+        }
+
+        public void StopCollecting()
+        {
+            if (controller != null)
+            {
+                controller.StopDevice();
+                controller = null;
+            }
+        }
+        public int[] GetRawData()
+        {
+            return dataCaptured;
+        }
+
+        private void GetMeasurements()
+        {
+            dataCaptured = new int[10];
+            System.Threading.ThreadPool.QueueUserWorkItem((dummy) =>
+            {
+                int x = 0;
+                Random timer = new Random();
+
+                while (controller != null)
+                {
+                    System.Threading.Thread.Sleep(timer.Next(1000, 5000));
+                    dataCaptured[x] = controller != null ? controller.TakeMeasurement() : dataCaptured[x];
+                    mostRecentMeasure = dataCaptured[x];
+
+                    x++;
+                    if (x == 10)
+                    {
+                        x = 0;
+                    }
+                }
+            });
+        }
+
+        protected Units unitsToUse;
+        protected int[] dataCaptured;
+        protected int mostRecentMeasure;
+        protected DeviceController controller;
+        protected DeviceType measurementType;
+    }
+}
---
+++ MeasuringDevice/MeasureLengthDevice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DeviceControl;
+
+namespace MeasuringDevice
+{
+    public class MeasureLengthDevice : MeasureDataDevice
+    {
+        public MeasureLengthDevice(Units deviceUnits)
+        {
+            unitsToUse = deviceUnits;
+            measurementType = DeviceType.LENGTH;
+        }
+
+        public override decimal MetricValue()
+        {
+            decimal metricMostRecentMeasure;
+
+            if (unitsToUse == Units.Metric)
+            {
+                metricMostRecentMeasure = Convert.ToDecimal(mostRecentMeasure);
+            }
+            else
+            {
+                decimal decimalImperialValue = Convert.ToDecimal(mostRecentMeasure);
+                decimal conversionFactor = 25.4M;
+                metricMostRecentMeasure = decimalImperialValue * conversionFactor;
+            }
+
+            return metricMostRecentMeasure;
+        }
+
+        public override decimal ImperialValue()
+        {
+            decimal imperialMostRecentMeasure;
+
+            if (unitsToUse == Units.Imperial)
+            {
+                imperialMostRecentMeasure = Convert.ToDecimal(mostRecentMeasure);
+            }
+            else
+            {
+                decimal decimalMetricValue = Convert.ToDecimal(mostRecentMeasure);
+                decimal conversionFactor = 0.3937M;
+                imperialMostRecentMeasure = decimalMetricValue * conversionFactor;
+            }
+
+            return imperialMostRecentMeasure;
+        }
+    }
+}
